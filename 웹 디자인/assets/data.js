6e0a3301839f230613c7ff09a7a4e0a26bf9649b
ensrate <ensrate@gmail.com>
2024-01-04 07:23:07 +0900

    .
    .

---
+++ 웹 디자인/assets/data.js
@@ -0,0 +1,51 @@
+export const data = [
+	{
+		title: `피부질환 확인`,
+		image: './assets/images/Deep_Learning.png',
+		color: '#f98866',
+		description: `딥러닝 모델을 사용하여 사용자님의 피부질환이 무엇인지 확인해드립니다`
+	},
+	{
+		title: `Kakao Map`,
+		image: './assets/images/kakaomap.png',
+		color: '#ffbb00',
+		description: `
+		<a href="http://host240102.dothome.co.kr/map/kakao_map (search).html" target="blank">카카오맵 바로가기</a>`
+	},
+	{
+		title: `Lorem ipsum`,
+		image: '',
+		color: '#80bd9e',
+		description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, officiis alias amet ab asperiores, tempora id nihil nam molestiae fugiat magnam commodi. Laboriosam sequi facilis tenetur temporibus deleniti enim ipsum, amet ipsa accusantium eum numquam iste laudantium maxime. Rerum facere eius blanditiis perspiciatis officia voluptates provident. Est velit aliquam vero iusto vel atque, ratione, reiciendis assumenda asperiores harum blanditiis inventore quidem nam non sint natus ipsam voluptate tenetur!`
+	},
+	{
+		title: `Lorem ipsum`,
+		image: '',
+		color: '#375e97',
+		description: `Tina's Portfolio ipsum dolor sit amet consectetur adipisicing elit. Ratione, officiis alias amet ab asperiores, tempora id nihil nam molestiae fugiat magnam commodi. Laboriosam sequi facilis tenetur temporibus deleniti enim ipsum, amet ipsa accusantium eum numquam iste laudantium maxime. Rerum facere eius blanditiis perspiciatis officia voluptates provident. Est velit aliquam vero iusto vel atque, ratione, reiciendis assumenda asperiores harum blanditiis inventore quidem nam non sint natus ipsam voluptate tenetur!`
+	},
+	{
+		title: 'Lorem ipsum',
+		image: '',
+		color: '#f98866',
+		description: `Butter ipsum dolor sit amet consectetur adipisicing elit. Ratione, officiis alias amet ab asperiores, tempora id nihil nam molestiae fugiat magnam commodi. Laboriosam sequi facilis tenetur temporibus deleniti enim ipsum, amet ipsa accusantium eum numquam iste laudantium maxime. Rerum facere eius blanditiis perspiciatis officia voluptates provident. Est velit aliquam vero iusto vel atque, ratione, reiciendis assumenda asperiores harum blanditiis inventore quidem nam non sint natus ipsam voluptate tenetur!`
+	},
+	{
+		title: 'Lorem ipsum',
+		image: '',
+		color: '#ffbb00',
+		description: `Colo Company ipsum dolor sit amet consectetur adipisicing elit. Ratione, officiis alias amet ab asperiores, tempora id nihil nam molestiae fugiat magnam commodi. Laboriosam sequi facilis tenetur temporibus deleniti enim ipsum, amet ipsa accusantium eum numquam iste laudantium maxime. Rerum facere eius blanditiis perspiciatis officia voluptates provident. Est velit aliquam vero iusto vel atque, ratione, reiciendis assumenda asperiores harum blanditiis inventore quidem nam non sint natus ipsam voluptate tenetur!`
+	},
+	{
+		title: 'Lorem ipsum',
+		image: '',
+		color: '#80bd9e',
+		description: `Jeju Island ipsum dolor sit amet consectetur adipisicing elit. Ratione, officiis alias amet ab asperiores, tempora id nihil nam molestiae fugiat magnam commodi. Laboriosam sequi facilis tenetur temporibus deleniti enim ipsum, amet ipsa accusantium eum numquam iste laudantium maxime. Rerum facere eius blanditiis perspiciatis officia voluptates provident. Est velit aliquam vero iusto vel atque, ratione, reiciendis assumenda asperiores harum blanditiis inventore quidem nam non sint natus ipsam voluptate tenetur!`
+	},
+	{
+		title: 'Lorem ipsum',
+		image: '',
+		color: '#375e97',
+		description: `Goooogle ipsum dolor sit amet consectetur adipisicing elit. Ratione, officiis alias amet ab asperiores, tempora id nihil nam molestiae fugiat magnam commodi. Laboriosam sequi facilis tenetur temporibus deleniti enim ipsum, amet ipsa accusantium eum numquam iste laudantium maxime. Rerum facere eius blanditiis perspiciatis officia voluptates provident. Est velit aliquam vero iusto vel atque, ratione, reiciendis assumenda asperiores harum blanditiis inventore quidem nam non sint natus ipsam voluptate tenetur!`
+	},
+];
